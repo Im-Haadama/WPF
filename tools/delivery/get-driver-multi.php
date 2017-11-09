52eab304d5be245fa2b2cdd20db03e3c525fc368
agla <yaakov.aglamaz@gmail.com>
2017-11-09 15:07:31 +0200

    1) Just before embedding short path 2) Driver output change - less information.
    Version 1.1:
    1) Coder: use config file for each object.
    2) Delivery: select prod from list, change order of lines. Add Collection.
    3) Mission: client now select day. Drivers get page per mission.
    4) Postcode: get postcode from Israel Post service.
    5) Sync zone between sites.
    6) Get-total-orders: create single supplier supplies. Send operation to post file.
    7) Supplies: finish implementation for units.
    8) Tasklist: prime version.
    9)
    
    Version 1.0.5:
    1) Save payment receipt with each delivery
    2) Require login.
    3) Count number of lines in order.
    4) St
    
    Version 1.0.4
    1) Save payment receipt number in delivery.
    2) Authrize use of scripts.
    3) Get invoice user by email.
    4) Set category from search tool.
    5) Stop setting supplier-category to products
    6) Count line number in delivery.
    7) Select term -> auto complete instead of selection.
    8) Add needed products cache. Not active for now.
    
    Version 1.0.3
    1) Makolet report.
    2) Rename business to post.php
    3) Add error info when connecting to invoice4u.
    4) Bug fix - calculate more the one basket in the delivery.
    5) Vat - default yes.
    6)
    
    Version 1.0.2
    1) Add category to pricelist.
    2) Escape ' in product name
    3) Fix a bug - qunatity discount line wasn't saved.
    4) Get the fresh category from wp_option table.
    5) delivery/test.php - to test fresh category.
    6) Get the user/password of the database from wp-config.

--- tools/delivery/get-driver-multi.php
+++ tools/delivery/get-driver-multi.php
@@ -6,8 +6,14 @@
  * Time: 22:41
  */
 
-require_once( '../tools_wp_login.php' );
+require_once( '../r-shop_manager.php' );
 require_once( '../multi-site/multi-site.php' );
+
+$debug = false;
+
+if ( isset( $_GET["debug"] ) ) {
+	$debug = true;
+}
 
 if ( isset( $_GET["week"] ) ) {
 	$week = $_GET["week"];
@@ -19,10 +25,9 @@ if ( date( 'Y-m-d' ) > date( 'Y-m-d', strtotime( $week . "+1 week" ) ) ) {
 	print gui_hyperlink( "שבוע הבא", "get-driver-multi.php?week=" . date( 'Y-m-d', strtotime( $week . " +1 week" ) ) ) . " ";
 }
 
-print gui_hyperlink( "שבוע קודם", "get-driver-multi.php?week=" . date( 'Y-m-d', strtotime( $week . " -1 week" ) ) );
+//print gui_hyperlink( "שבוע קודם", "get-driver-multi.php?week=" . date( 'Y-m-d', strtotime( $week . " -1 week" ) ) );
 
 $output = MultiSite::GetAll( "delivery/get-driver.php?week=" . $week );
-
 
 $dom = str_get_html( $output );
 
@@ -35,12 +40,15 @@ print "}";
 print "</style>";
 
 $data_lines = array();
+
 $header     = null;
 foreach ( $dom->find( 'tr' ) as $row ) {
 	if ( ! $header ) {
-		for ( $i = 0; $i < 8; $i ++ ) {
+		for ( $i = 0; $i < 7; $i ++ ) {
 			$header .= $row->find( 'td', $i );
 		}
+		$header .= gui_cell( gui_header( 3, "מספר ארגזים, קירור" ) );
+		$header .= gui_cell( gui_header( 3, "נמסר" ));
 		continue;
 	}
 	// $key_fields = $row->find( 'td', 11 )->plaintext;
@@ -61,9 +69,15 @@ foreach ( $dom->find( 'tr' ) as $row ) {
 	// print "name = " . $name . " key= "  . $key . "<br/>";
 	$mission_id = $row->find( 'td', 8 )->plaintext;
 	$line_data  = "<tr>";
-	for ( $i = 0; $i < 8; $i ++ ) {
+	for ( $i = 0; $i < 7; $i ++ ) {
 		$line_data .= $row->find( 'td', $i );
 	}
+	$line_data .= gui_cell( "" ); // #box
+	$line_data .= gui_cell( gui_checkbox( "", "" ) ); // #delivered
+	if ( $debug ) {
+		$line_data .= gui_cell( $key . " " . $long . " " . $lat );
+	}
+
 	$line_data .= "</tr>";
 	if ( ! isset( $data_lines[ $mission_id ] ) ) {
 		$data_lines[ $mission_id ] = array();
@@ -77,6 +91,9 @@ foreach ( $data_lines as $mission_id => $data_line ) {
 	// print "mission_id: " . var_dump($data_lines[$mission_id]) . "<br/>";
 	print "<table>";
 	$data = $header;
+
+	$data .= gui_list( "באחריות הנהג להעמיס את הרכב ולסמן את מספר האריזות והאם יש קירור." );
+	$data .= gui_list( "יש לוודא שכל המשלוחים הועמסו.");
 
 	sort( $data_line );
 
