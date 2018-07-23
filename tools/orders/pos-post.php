f76d821e9cb50c155e1ff840043425c9dcdcc8e6
agla <yaakov.aglamaz@gmail.com>
2018-07-23 17:55:27 +0300

    Version 1.4: 1) POS - create order delivery and pay - all in one. 2) Create agla folder from fundamentals. 3) Inventory report. 4) Managed fresh stock. 5) Get price by customer type and quantity. 6) Get employee basket payment from differnt site. 7) Show inventory. 8) Version 1.3: 1) Im haadama closure - enable delete price list.
    Version 1.2:
    1) Silence php warnings.
    2) Change cash delta limit to 400.
    3) Bundles comeback!
    4) Fresh Siton Catalog.
    5) Worker discount.
    6) Fix role requirements for internal functions.
    7) Fix route to be base on order and not client.
    8) Update pricelist from xls - for chef site.
    
    Version 1.1:
    1) Coder: use config file for each object.
    2) Delivery: select prod from list, change order of lines. Add Collection.
    3) Mission: client now select day. Drivers get page per mission.
    4) Postcode: get postcode from Israel Post service.
    5) Sync zone between sites.
    6) Get-total-orders: create single supplier supplies. Send operation to post file.
    7) Supplies: finish implementation for units.
    8) Tasklist: prime version.
    9)
    
    Version 1.0.5:
    1) Save payment receipt with each delivery
    2) Require login.
    3) Count number of lines in order.
    4) St
    
    Version 1.0.4
    1) Save payment receipt number in delivery.
    2) Authrize use of scripts.
    3) Get invoice user by email.
    4) Set category from search tool.
    5) Stop setting supplier-category to products
    6) Count line number in delivery.
    7) Select term -> auto complete instead of selection.
    8) Add needed products cache. Not active for now.
    
    Version 1.0.3
    1) Makolet report.
    2) Rename business to post.php
    3) Add error info when connecting to invoice4u.
    4) Bug fix - calculate more the one basket in the delivery.
    5) Vat - default yes.
    6)
    
    Version 1.0.2
    1) Add category to pricelist.
    2) Escape ' in product name
    3) Fix a bug - qunatity discount line wasn't saved.
    4) Get the fresh category from wp_option table.
    5) delivery/test.php - to test fresh category.
    6) Get the user/password of the database from wp-config.

---
+++ tools/orders/pos-post.php
@@ -0,0 +1,116 @@
+<?php
+
+ini_set( 'display_errors', 'on' );
+
+/**
+ * Created by PhpStorm.
+ * User: agla
+ * Date: 21/06/18
+ * Time: 13:24
+ */
+
+require_once( "../im_tools.php" );
+require_once( 'orders-common.php' );
+require_once( "../delivery/create-delivery-post.php" );
+require_once( "../account/account-post.php" );
+
+$order_id  = $_GET["order_id"];
+$operation = $_GET["operation"];
+
+//$operation = $_GET["operation"];
+my_log( "Operation: " . $operation, __FILE__ );
+switch ( $operation ) {
+	case "pay_cash":
+//		$order_id = $_GET["order_id"];
+		$cash = 0;
+		if ( isset( $_GET["cash"] ) ) {
+			$cash = $_GET["cash"];
+		}
+		$bank = 0;
+		if ( isset( $_GET["bank"] ) ) {
+			$bank = $_GET["bank"];
+		}
+		$check = 0;
+		if ( isset( $_GET["check"] ) ) {
+			$check = $_GET["check"];
+		}
+		$credit = 0;
+		if ( isset( $_GET["credit"] ) ) {
+			$credit = $_GET["credit"];
+		}
+		$change = 0;
+		if ( isset( $_GET["change"] ) ) {
+			$change = $_GET["change"];
+		}
+		$user_id = $_GET["user_id"];
+		$del_id  = $_GET["del_id"];
+
+		create_receipt( $cash, $bank, $check, $credit, $change, $user_id, date( 'Y-m-d' ), array( $del_id ) );
+		break;
+
+	case "create_delivery":
+//		$order_id = $_GET["order_id"];
+
+		create_delivery( $order_id );
+		break;
+
+	case "close_order":
+		$ids = $_GET["ids"];
+		order_change_status( explode( ",", $ids ), "wc-completed" );
+		break;
+
+}
+
+//function pos_pay($order_id, $cash, $bank, $check, $credit, $change)
+//{
+//
+//	$delivery_id = create_delivery($order_id);
+//
+//	$u = order_get_customer_id($order_id);
+//	print "user: " . $u . "<br/>";
+//	create_receipt($cash, $bank, $check, $credit, $change, $u, date('Y-m-d'), array($delivery_id));
+//}
+
+function create_delivery( $order_id ) {
+	$prods       = array();
+	$order       = new WC_Order( $order_id );
+	$order_items = $order->get_items();
+	$total       = 0;
+	$vat         = 0;
+	$lines       = 0;
+	foreach ( $order_items as $product ) {
+		$lines ++;
+		$p = $product['price'];
+		// push_array($prods, array($product['qty']));
+		// $total += $p * $q;
+		// var_dump($product);
+		$prod                     = array();
+		$prod['product_name']     = $product["name"];
+		$prod['quantity']         = $product["quantity"];
+		$prod['quantity_ordered'] = 0;
+		$prod['vat']              = 0;
+		$prod['price']            = $product['total'] / $product["quantity"];
+		$prod['line_price']       = $product['total'];
+		$total                    += $product['total'];
+		$prod['prod_id']          = $product['product_id'];
+
+		// var_dump($prod);
+		array_push( $prods, $prod );
+	}
+
+	$delivery_id = create_delivery_header( $order_id, $total, $vat, $lines, false, 0 );
+
+	print "תעודת משלוח מספר " . $delivery_id;
+
+	foreach ( $prods as $prod ) {
+		add_delivery_line( $prod['product_name'], $delivery_id, $prod['quantity'], $prod['quantity_ordered'], 0,
+			$prod['vat'], $prod['price'], $prod['line_price'], $prod['prod_id'] );
+	}
+
+	print " נוצרה <br/>";
+
+//	$order = new WC_Order( $order_id );
+//	$order->update_status( 'wc-completed' );
+
+	return $delivery_id;
+}
