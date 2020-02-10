68b774c90bf3a63438eb40c8b6831a968c35a677
vivek <vivek@webtechsystem.com>
2020-02-10 16:29:26 +0530

    Search results: show the results like category display - 4 products a line, with price and controlles to add it to basket No need for author, date fields, comments that displayed now (ID 3709). : DONE

--- wp-content/plugins/fresh/fresh.php
+++ wp-content/plugins/fresh/fresh.php
@@ -504,7 +504,6 @@ function woocommerce_form_field_radio( $key, $args, $value = '' ) {
 }
 
 
-
 function im_custom_order_button_text() {
 	return __( 'אשר הזמנתך', 'woocommerce' );
 }
@@ -623,3 +622,75 @@ function pay_page($atts, $content = null)
 //        print "need to login";
 //    }
 }
+
+
+
+
+
+/*-- Start product search filter --*/
+function searchfilter($query) {
+ 
+    if ($query->is_search && !is_admin() ) {
+        $query->set('post_type',array('product'));
+    }
+ 
+    return $query;
+}
+ 
+add_filter('pre_get_posts','searchfilter');
+
+/*-- End product search filter --*/
+
+
+/*-- Start custom add to cart search product --*/
+
+add_action('wp_ajax_ql_woocommerce_ajax_add_to_cart', 'sm_woocommerce_ajax_add_to_cart'); 
+add_action('wp_ajax_nopriv_ql_woocommerce_ajax_add_to_cart', 'sm_woocommerce_ajax_add_to_cart');          
+
+function sm_woocommerce_ajax_add_to_cart() {  
+
+    $product_id = apply_filters('ql_woocommerce_add_to_cart_product_id', absint($_POST['product_id'])); 
+    
+    $quantity = empty($_POST['quantity']) ? 1 : wc_stock_amount($_POST['quantity']); 
+
+    $variation_id = absint($_POST['variation_id']); 
+
+    $passed_validation = apply_filters('ql_woocommerce_add_to_cart_validation', true, $product_id, $quantity); 
+
+    $product_status = get_post_status($product_id); 
+
+    if ($passed_validation && WC()->cart->add_to_cart($product_id, $quantity, $variation_id) && 'publish' === $product_status) { 
+
+        do_action('ql_woocommerce_ajax_added_to_cart', $product_id); 
+
+            if ('yes' === get_option('ql_woocommerce_cart_redirect_after_add')) { 
+
+                wc_add_to_cart_message(array($product_id => $quantity), true); 
+
+            } 
+
+            WC_AJAX :: get_refreshed_fragments(); 
+
+    } else { 
+
+        $data = array( 
+
+            'error' => true, 
+
+            'product_url' => apply_filters('ql_woocommerce_cart_redirect_after_error', get_permalink($product_id), $product_id)); 
+
+        echo wp_send_json($data); 
+
+    } 
+
+    wp_die(); 
+
+}
+
+
+function custom_enqueue_script() {   
+    wp_enqueue_script( 'my_custom_script', plugin_dir_url( __FILE__ ) . 'js/add_to_cart_on_serach.js' );
+}
+add_action('wp_enqueue_scripts', 'custom_enqueue_script');
+
+/*-- End custom add to cart search product --*/
---
+++ wp-content/themes/minezine/content-product-archives.php
@@ -0,0 +1,63 @@
+<?php
+/**
+* The template for displaying content of search/archives.
+* @package MineZine
+* @since MineZine 1.0.0
+*/
+?>
+<li class="product type-product post-<?php echo get_the_ID(); ?> status-publish first instock product_cat-fruits has-post-thumbnail shipping-taxable purchasable product-type-simple">  
+  <?php
+    $product_id = get_the_ID();
+    $product = wc_get_product( $product_id );
+    $currency = get_woocommerce_currency_symbol();
+  ?>
+
+  <a href="<?php echo get_permalink(); ?>" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+    <?php if( $product->is_on_sale() ) { 
+    echo '<span class="onsale"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">s a l e!</font></font></span>';
+    }
+    ?>
+    <?php if ( has_post_thumbnail() ) { 
+    the_post_thumbnail(); 
+    } ?>
+    <h2 class="woocommerce-loop-product__title"><?php the_title(); ?></h2> 
+    <span class="price">
+      <?php 
+      if( $product->is_type( 'simple' ) ){ 
+        $sale_price = $product->get_sale_price();
+        $reg_price = $product->get_regular_price();
+        if( $product->is_on_sale() ) {
+         echo '<span class="woocommerce-Price-amount amount block_price"><span class="woocommerce-Price-currencySymbol">'.$currency.'</span>'.$reg_price.'</span> <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">'.$currency.'</span>'.$sale_price.'</span>';
+        }else{
+          echo '<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">'.$currency.'</span>'.$reg_price.'</span>';
+       }  
+      } elseif( $product->is_type( 'variable' ) ){ 
+        $min_price = $product->get_variation_price( 'min' );
+        $max_price = $product->get_variation_price( 'max' );
+        if($min_price != $max_price) {
+          echo '<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">'.$currency.'</span>'.$min_price.'</span> - <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">'.$currency.'</span>'.$max_price.'</span>';
+        }else{
+          $price_range = esc_attr($currency).''.$min_price; 
+          echo '<span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">'.$currency.'</span>'.$min_price.'</span>';
+        }
+      } ?>
+  </span>
+  </a>          
+  <?php 
+  if( $product->is_type( 'simple' ) ){  ?>
+
+    <form class="cart" id="crt-form"  method="post" enctype="multipart/form-data">
+      <input type="hidden" name="product_id" id="product_id" value="<?php $product_id; ?>">
+      <input style="display: none;" type="number" step="1" min="1" name="quantity" value="1" title="Qty" class="input-text qty text">
+      <button type="submit" name="add-to-cart" value="<?php echo $product_id; ?>" class="button product_type_simple add_to_cart_button ajax_add_to_cart srh_addto_cart"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+    </form> 
+    <?php 
+  } elseif( $product->is_type( 'variable' ) ){
+    $min_price = $product->get_variation_price( 'min' );
+    $max_price = $product->get_variation_price( 'max' );
+    $price_range = esc_attr($currency).''.$min_price.' - '.esc_attr($currency).''.$max_price;
+    ?>
+    <a href="<?php echo get_permalink($product_id); ?>" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart srh_addto_cart" data-product_id="8" data-product_sku="" aria-label="Add “banana” to your cart" rel="nofollow">Select options</a>
+    <?php 
+  } ?>
+</li>
