a87c50150c38d0e3ada489be48dd423462b2c541
agla <yaakov.aglamaz@gmail.com>
2020-05-21 19:55:02 +0300

    Somehow product_comment was translated to הערות. Allow close order without delivery fee. Update cart for siton/owner
    Signed-off-by: agla <yaakov.aglamaz@gmail.com>

--- wp-content/plugins/fresh/includes/class-fresh-order-management.php
+++ wp-content/plugins/fresh/includes/class-fresh-order-management.php
@@ -16,129 +16,16 @@ class Fresh_Order_Management {
 		$this->version     = '1.0';
 
 		add_action( 'wp_enqueue_scripts', array($this, 'enqueue_scripts' ));
-
 	}
 
 	static public function init_hooks() {
-//		MyLog(debug_trace(5));
 //		MyLog(__FUNCTION__ . __CLASS__);
 		// add_filter( 'manage_shop_order_posts_custom_column', array(__CLASS__, 'add_my_account_order_actions'), 10, 2 );
 		add_filter('woocommerce_admin_order_actions', array(__CLASS__, 'add_order_action'), 10, 2);
 		add_filter('order_complete', array(__CLASS__, 'order_complete_wrap'));
 		add_action('admin_post_delivery', array(__CLASS__, 'create_delivery_note'));
 		add_action( 'woocommerce_view_order', array(__CLASS__, 'show_edit_order'), 10 );
-		// https://wordpress.stackexchange.com/questions/357948/how-to-update-the-order-items-while-editing-an-order-on-the-woocommerce-admin-or
 
-		// update siton prices in cart.
-		AddAction('woocommerce_order_before_calculate_totals', array(__CLASS__, 'calculate_by_customer_type'), 10, 3);
-//		AddAction( 'woocommerce_before_calculate_totals', 'im_woocommerce_update_price', 99 );
-
-		AddAction( 'woocommerce_before_calculate_totals', array(__CLASS__, 'update_cart'), 99 );
-
-//		AddAction('woocommerce_before_calculate_totals', array(__CLASS__, 'update_cart'), 10, 1);
-////		add_action( 'woocommerce_before_calculate_totals', array(__CLASS__, 'im_woocommerce_update_price'), 10, 2 );
-	}
-
-	static function update_cart(WC_Cart $cart)
-	{
-		// Trace the cart
-		$message = "cart start " . $_SERVER['REMOTE_ADDR'] . " ";
-		if (function_exists('get_user_id') and get_user_id() ) {
-			$f = new Fresh_Client(get_user_id());
-			$message .= "user: " . $f->getName();
-		}
-		MyLog( $message);
-
-		if (! SqlQuery("select 1")) {
-			MyLog ("not connected to db");
-			return;
-		}
-		$C = new Fresh_Client(get_user_id());
-		$client_type = $C->customer_type();
-
-		$message = "Cart: ";
-
-		foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
-			$prod_id = $cart_item['product_id'];
-			$variation_id = $cart_item['variation_id'];
-			if ( ! ( $prod_id > 0 ) ) {
-				$message .= _( "cart - no prod_id" );
-				continue;
-			}
-			$q          = $cart_item['quantity'];
-			$f = new Fresh_Product($prod_id);
-			if ($f->is_basket()) { $message .= "basket skipped"; continue; }
-			$sell_price = Fresh_Pricing::get_price_by_type($prod_id, $client_type, $q, $variation_id );
-			//my_log("set " . $sell_price);
-			$cart_item['data']->set_sale_price( $sell_price );
-			$cart_item['data']->set_price( $sell_price );
-			$message .= $prod_id ." " . $q;
-		}
-		MyLog($message);
-//		if (! function_exists('get_user_id')) return $cart;
-//
-//		if (get_user_id() != 474) return;
-//		MyLog(__FUNCTION__. " " . get_user_id());
-//		$customer_id = get_user_id();
-//		if (! $customer_id) {
-//			MyLog( "no customer id" );
-//			return;
-//		}
-//		$f = new Fresh_Client($customer_id);
-//		$customer_type = $f->customer_type();
-//		MyLog("calculate cart ");
-//		foreach( $cart->get_cart() as $key => $item ) {
-//			WC()->cart_contents[ $key ]['line_total'] = 22;
-////			if ('WC_Order_Item_Product' != get_class($item)) {
-////				Mylog (get_class($item));
-////				continue;
-////			}
-//
-//			MyLog(StringVar($item['data']));
-//			return $cart;
-//			// get the WC_Product object
-////			$product = $item->get_product();
-////
-////			// get the quantity
-////			$product_quantity = $item->get_quantity();
-////
-////			if ($customer_type) {
-////				$price = Fresh_Pricing::get_price_by_type($product->id, $customer_type);
-////
-////				$item->set_total($product_quantity * $price);
-////			}
-//		}
-//		return $cart;
-	}
-	static function calculate_by_customer_type($and_taxes, WC_Order $order)
-	{
-		MyLog(__FUNCTION__. " " . $order->customer_id);
-		$customer_id = $order->customer_id;
-		if (! $customer_id) {
-			MyLog( "no customer id" );
-			return;
-		}
-		$f = new Fresh_Client($customer_id);
-		$customer_type = $f->customer_type();
-		MyLog("calculate cart order " . $order->get_id() . " user $customer_id type: $customer_type");
-		foreach( $order->get_items() as $item_id => $item ) {
-			if ('WC_Order_Item_Product' != get_class($item)) {
-				Mylog (get_class($item));
-				continue;
-			}
-
-			// get the WC_Product object
-			$product = $item->get_product();
-
-			// get the quantity
-			$product_quantity = $item->get_quantity();
-
-			if ($customer_type) {
-				$price = Fresh_Pricing::get_price_by_type($product->id, $customer_type);
-
-				$item->set_total($product_quantity * $price);
-			}
-		}
 	}
 
 	static public function show_edit_order($order_id)
@@ -243,15 +130,10 @@ class Fresh_Order_Management {
 			if (! $fee) {
 				MyLog("No delivery fee for order $order_id");
 				Fresh::instance()->add_admin_notice("No delivery fee for order $order_id");
-//				 Change back the order status.
+				// Change back the order status.
 			}
 
 			$del_id = Fresh_Delivery::CreateDeliveryFromOrder($order_id, 1);
-			$d = new Fresh_Delivery($del_id);
-			$admin_email = get_bloginfo('admin_email');
-			if (defined('ADMIN_MAIL')) $admin_email = ADMIN_MAIL;
-
-			$d->send_mail($admin_email);
 		} else {
 			MyLog ("have del: " . $O->getDeliveryId());
 		}
@@ -282,11 +164,3 @@ class Fresh_OrderFields {
 //		percentage = 13,
 		field_count = 9;
 }
-
-//add_filter( 'woocommerce_cart_product_subtotal', 'modify_cart_product_subtotal', 10, 4 );
-//function modify_cart_product_subtotal( $product_subtotal, $product, $quantity, $cart ) {
-//	$product_subtotal = 22;
-//	// Add your logic here.
-//	// You can use the $cart instead of using the global $woocommerce variable.
-//	return $product_subtotal;
-//}
