ebc9ab69d6ddb6b9439ea99542b0716b3d72d022
agla <yaakov.aglamaz@gmail.com>
2020-12-01 10:49:24 +0200

    Finance account: - add create invoice. - change the form of the
    Signed-off-by: agla <yaakov.aglamaz@gmail.com>

--- wp-content/plugins/finance/includes/class-finance.php
+++ wp-content/plugins/finance/includes/class-finance.php
@@ -198,6 +198,7 @@ class Finance {
 		$this->loader->AddAction("get_open_trans", $this, 'get_open_trans');
 		$this->loader->AddAction("exists_invoice", $this, 'exists_invoice');
 		$this->loader->AddAction("create_receipt", $this, 'create_receipt');
+		$this->loader->AddAction("create_invoice", $this, 'create_invoice');
 
 		if ((get_user_id() == 1) and defined("DEBUG_USER")) wp_set_current_user(DEBUG_USER);
 
@@ -444,6 +445,18 @@ class Finance {
 		die ( 1 );
 	}
 
+	function create_invoice() {
+		$row_ids = GetParamArray( "row_ids" );
+		$user_id = GetParam( "user_id", true );
+		$date    = GetParam( "date" );
+
+		$doc_id = Finance_Clients::create_invoice_from_account_ids( $user_id, $date, $row_ids );
+		// print "doc=$doc_id<br/>";
+		print $doc_id;
+		die ( 1 );
+	}
+
+
 	static function get_open_trans() {
 		$multi_site = Core_Db_MultiSite::getInstance();
 
@@ -460,7 +473,7 @@ class Finance {
 		return true;
 	}
 
-static function get_open_site_invoices()
+	static function get_open_site_invoices()
 	{
 		$debug = GetParam("debug");
 		$sum         = array();
@@ -888,12 +901,12 @@ static function get_open_site_invoices()
 
 	static function Invoice4uConnect()
 	{
-		MyLog(__FUNCTION__);
+		FinanceLog(__FUNCTION__);
 		if ($i = Finance_Invoice4u::getInstance()) return $i;
-		MyLog("Connecting " . INVOICE_USER);
+		FinanceLog("Connecting " . INVOICE_USER);
 		if (defined('INVOICE_USER') and defined('INVOICE_PASSWORD'))
 			return new Finance_Invoice4u(INVOICE_USER, INVOICE_PASSWORD);
-		else MyLog("No invoice user or password");
+		else FinanceLog("No invoice user or password");
 
 		return null;
 	}
---
+++ wp-content/plugins/instagram/instagram.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: instagram
+ * Plugin URI: https://e-fresh.co.il
+ * Description: Instagram crawler
+ * Version: 1.0
+ * Author: agla
+ * Author URI: http://e-fresh.co.il
+ * Text Domain: e-fresh
+ *
+ * @package Instagram
+ */
+
+//require_once (ABSPATH . '/vendor/Instagram.php');
+//
+//use MetzWeb\Instagram\Instagram;
+//
+//$instagram = new Instagram(array(
+//	'apiKey' => 'YOUR_APP_KEY',
+//	'apiSecret' => 'YOUR_APP_SECRET',
+//	'apiCallback' => 'YOUR_APP_CALLBACK' // must point to success.php
+//));
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$AppID = "1360571697612487";
+$secret = "095913da417ebb02b1516724c5c172ed";
+
+$token = file_get_contents("https://graph.facebook.com/oauth/access_token?client_id=$AppID&client_secret=$secret&grant_type=client_credentials");
+
+"https://developers.facebook.com/docs/instagram-api/guides/hashtag-search/"
+//var_dump($token);
