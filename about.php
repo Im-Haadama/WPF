e2c3229cb42419424475c366d763f74de5625605
agla <yaakov.aglamaz@gmail.com>
2021-06-21 16:56:01 +0300

    try wordpress quota functions.
    Signed-off-by: agla <yaakov.aglamaz@gmail.com>

--- about.php
+++ about.php
@@ -1,3 +1,13 @@
 <?php
-//print phpinfo();
-print xdebug_info();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once "wp-load.php";
+require_once "wp-includes/ms-functions.php";
+require_once "wp-admin/includes/ms.php";
+
+print "allowed space: " . get_space_allowed() . "<br/>";
+print "used: " . display_space_usage() . "<br/>";
+print phpinfo();
+//print xdebug_info();
